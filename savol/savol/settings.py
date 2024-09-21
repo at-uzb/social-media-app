8ea1505b855e4e73c16c3fb4cbd298109127fb0a
Abdulaziz <74441512+at-uzb@users.noreply.github.com>
2024-09-21 11:48:26 +0500

    Update settings.py

--- savol/savol/settings.py
+++ savol/savol/settings.py
@@ -161,5 +161,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tothebest2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'wogm funi bhfd xrts'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
